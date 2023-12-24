b32058cc81ada4595061342fa4cf60be04979501
raisabanu <hieitsraisu@gmail.com>
2023-12-24 18:16:09 -0500

    Variables and DataTypes

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log("Welcome to the world of JavaScript!!");
+
+
+//you can never reassign if the variable is a constatnt
+const varname1 = 'Hello!';
+console.log(varname1);
+const $name = 'Raisa';
+console.log($name);
+
+//Single line comment
+//Creating a variable using var keyword
+var variablename = 'test';
+console.log(variablename);
+
+//creating a variable using let keyword
+let varname3 = 'test'
+console.log('varname3');
+
+//Variable Nameing
+//1.The name should be unique
+//2.The names should not be any reserved keyword.
+//3.The names must stART with a character or a _ or a $ sign
+
+console.log('varname1');
+
+
+//String
+//datatype to represent text
+//type of variables in string 
+//single quote, double quote both are identical.
+//back ticks string is complex string 
+//backticks provide extended functionality.
+//They allow us to embedd the variables and expressions into string by wrapping them with $sign and {}
+
+const exampleString = 'Example String';
+console.log(exampleString);
+
+const singlequotes = 'Single Quote String value';
+console.log(singlequotes);
+
+const doublequotes = 'Double Quote String value';
+console.log(doublequotes);
+
+const myname ='Raisa';
+//const backticks = `Hello!, ${myname}, Welcome to JS session!`;
+const backticks = `The Sum of two numbers is ${2 + 2}!`;
+console.log(backticks);
+
